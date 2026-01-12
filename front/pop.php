cfdecaeab1607510ceeaffeebd5f76290108169d
mackliu <macklun@gmail.com>
2026-01-12 16:01:08 +0800

    完成最新文章及人氣文章列及分類功能

---
+++ front/pop.php
@@ -0,0 +1,52 @@
+<fieldset>
+    <legend>目前位置：首頁 > 人氣文章區</legend>
+
+
+<table>
+    <tr>
+        <td style="width:30%">標題</td>
+        <td style="width:50%">內容</td>
+        <td></td>
+    </tr>
+    <?php
+    $total=$Post->count(['sh'=>1]);
+    $div=5;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+
+    $posts=$Post->all(['sh'=>1]," order by `good` desc limit $start,$div");
+    foreach($posts as $post):
+    ?>
+    <tr>
+        <td><?=$post['title'];?></td>
+        <td><?=mb_substr($post['text'],0,25);?>...</td>
+        <td></td>
+    </tr>
+    <?php
+    endforeach;
+    ?>
+</table>
+<div>
+   <?php 
+   if($now-1>0){
+        $prev=$now-1; 
+        echo "<a href='?do=pop&p=$prev'> < </a>";
+   }
+
+   for($i=1;$i<=$pages;$i++){
+        $font=($i==$now)?"24px;":"16px;";
+        echo "<a href='?do=pop&p=$i' style='font-size:$font'> $i </a>";
+   }
+
+   if($now+1<=$pages){
+        $next=$now+1;
+        echo "<a href='?do=pop&p=$next'> > </a>";
+   }
+   
+
+   ?>
+</div>
+
+
+</fieldset>
