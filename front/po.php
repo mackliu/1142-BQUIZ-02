93f596a05d2495b358ff865a51ba089b33c39305
mackliu <macklun@gmail.com>
2026-01-12 14:37:56 +0800

    建立分類網誌版型及更換導覽文字功能

---
+++ front/po.php
@@ -0,0 +1,26 @@
+<style>
+    .po-item{
+        display:block;
+        margin:5px;
+    }
+</style>
+<div>
+    目前位置：首頁 > 分類網誌 > <span id="nav">健康新知</span>
+</div>
+<fieldset style='width:20%;padding:10px;vertical-align:top;display:inline-block'>
+    <legend>分類網誌</legend>
+<a href="#" class="po-item" data-id="1">健康新知</a>
+<a href="#" class="po-item" data-id="2">菸害防治</a>
+<a href="#" class="po-item" data-id="3">癌症防治</a>
+<a href="#" class="po-item" data-id="4">慢性病防治</a>
+</fieldset>
+<fieldset style="width:70%;padding:10px;display:inline-block">
+    <legend>文章列表</legend>
+</fieldset>
+
+<script>
+$(".po-item").on("click",function(){
+    let item=$(this).text();
+    $("#nav").text(item);
+})
+</script>
