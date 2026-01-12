c77d97cc6ef50657be0cb0638d458a8e3e5f50eb
mackliu <macklun@gmail.com>
2026-01-12 10:49:28 +0800

    建立新增問卷功能頁面

--- back/que.php
+++ back/que.php
@@ -1,1 +1,30 @@
-問卷管理
+<fieldset>
+    <legend>新增問卷</legend>
+    <form action="./api/add_que.php" method="post">
+        <div>
+            <label for="">問卷名稱</label>
+            <input type="text" name="subject" id="">
+        </div>
+        <div id="options">
+            <div>
+                <label for="">選項</label>
+                <input type="text" name="opt[]" >
+                <input type="button" value="更多" onclick="more()">
+            </div>
+        </div>
+        <input type="submit" value="新增">|
+        <input type="reset" value="清空">
+    </form>
+</fieldset>
+
+
+
+<script>
+    function more(){
+        let opt=`<div>
+                    <label for="">選項</label>
+                    <input type="text" name="opt[]" >
+                 </div>`
+        $("#options").prepend(opt);
+    }
+</script>
