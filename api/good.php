2df59e0fb52b299f2984bd697b5300e800f624b7
mackliu <macklun@gmail.com>
2026-01-13 10:24:11 +0800

    最新文章按讚時,同時更新文章在資料表中的按讚數

--- api/good.php
+++ api/good.php
@@ -1,11 +1,17 @@
 <?php
 include_once "db.php";
 $post_id=$_POST['id'];
+$post=$Post->find($post_id);
+
 $member_id=$Mem->find(['acc'=>$_SESSION['login']])['id'];
 
 if($Log->count(['post_id'=>$post_id,'member_id'=>$member_id])>0){
     $Log->del(['post_id'=>$post_id,'member_id'=>$member_id]);
+    $post['good']--;
 }else{
     $Log->save(['post_id'=>$post_id,'member_id'=>$member_id]);
+    $post["good"]++;
 }
+
+$Post->save($post);
 
