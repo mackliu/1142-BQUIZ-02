b532aa840a1299a8837e37118691e7658df5f4ad
mackliu <macklun@gmail.com>
2026-01-13 11:01:47 +0800

    完成問卷調查頁面問卷列表

---
+++ front/que.php
@@ -0,0 +1,37 @@
+<fieldset>
+    <legend>目前位置：首頁 > 問卷調查</legend>
+
+    <table style="width:90%;margin:auto">
+        <tr class='ct'>
+            <td>編號</td>
+            <td style='width:70%'>問卷題目</td>
+            <td>投票總數</td>
+            <td>結果</td>
+            <td>狀態</td>
+        </tr>
+        <?php 
+        $ques=$Que->all(['main_id'=>0]);
+        foreach($ques as $idx => $que):
+        ?>
+        <tr>
+            <td><?=$idx+1;?></td>
+            <td><?=$que['text'];?></td>
+            <td><?=$que['vote'];?></td>
+            <td>
+                <a href="">結果</a>
+            </td>
+            <td>
+                <?php
+                if(isset($_SESSION['login'])){
+                    echo "<a href='#'>參與投票</a>";
+                }else{
+                    echo "請先登入";
+                }
+                ?>
+            </td>
+        </tr>
+        <?php 
+        endforeach;
+        ?>
+    </table>
+</fieldset>
